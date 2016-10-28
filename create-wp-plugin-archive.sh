a9480eef8a5db570548b6fd81f3dec9ba2a71dcb
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-10-28 09:17:47 +0200

    let create_wp-plugin-archive script use latest git tag name.

--- create-wp-plugin-archive.sh
+++ create-wp-plugin-archive.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-# Zip "Add Media Buttons" Wordpress Plugin
+# Zip "Add Media Buttons" Wordpress Plugin, with latest tag name contained in filename.
 
-cd add-media-buttons && zip -r ../wp-add-media-buttons.zip .
+cd add-media-buttons && zip -r ../wp-add-media-buttons-`git describe --abbrev=0`.zip .
